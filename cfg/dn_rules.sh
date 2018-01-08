95fc3d66e679a768c7cc712ee9ccf49892e86262
Garrick He <garrick_he@dell.com>
2018-01-08 15:57:55 -0800

    * Update: Modified ebtable rules to accept/forward IGMP packets * Update: Accept only marked IGMP packets * Update: Avoiding the VRRP MAC packets flooding in the kernel bridging level
    Change-Id: Ic59c261d6fc5dddcfdf0858b5a7f71c18d5b5b7e
    Signed-off-by: Garrick He <garrick_he@dell.com>

--- cfg/dn_rules.sh
+++ cfg/dn_rules.sh
@@ -16,7 +16,26 @@
 #
 
 ebtables -I FORWARD -d BGA -j DROP
-ebtables -A FORWARD -d Multicast -j DROP
+#Mark all multicast and broadcast packets in forwarding chain
+#and drop in the egress side. In some cases like
+#IGMP and MLD snooping the control packets needs to be
+#forwarded or flooded. So mark packets so that dropping
+#in egress affects only the forwarded packets and not
+#locally generated packets. Here packets can be tagged and untagged
+#so there is no rule which can accept both tagged and untagged IGMP/MLD packets.
+#So here IGMP/MLD rule is not added to accept it. The rule is added below in
+#egress after POSTROUTING.
+ebtables -A FORWARD -d Multicast -j mark --mark-set 0x1 --mark-target CONTINUE
+ebtables -A FORWARD -d Broadcast -j mark --mark-set 0x2 --mark-target CONTINUE
+#Accept/forward IGMP/MLD(Query - 130, Report - 131,Leave -132) packets
+#for snooping and rest marked with "0x1" drop it.
+ebtables -t nat -I POSTROUTING -p IPv4 --ip-proto igmp --mark 0x1 -j ACCEPT
+ebtables -t nat -I POSTROUTING -p IPv6 --ip6-proto ipv6-icmp --ip6-icmp-type 130 --mark 0x1 -j ACCEPT
+ebtables -t nat -I POSTROUTING -p IPv6 --ip6-proto ipv6-icmp --ip6-icmp-type 131 --mark 0x1 -j ACCEPT
+ebtables -t nat -I POSTROUTING -p IPv6 --ip6-proto ipv6-icmp --ip6-icmp-type 132 --mark 0x1 -j ACCEPT
+ebtables -t nat -A POSTROUTING -d Broadcast --mark 0x2 -j DROP
+ebtables -t nat -A POSTROUTING -d Multicast --mark 0x1 -j DROP
+
 ebtables -A FORWARD -p ARP -j DROP
 
 #Accept ARP request on eth0 (untagged/tagged interfaces) and don't copy it to NFLOG
@@ -26,3 +45,16 @@ ebtables -A OUTPUT -p ARP --arp-op Request --nflog-group 100 -j DROP
 # stop FCoE and FIP Packets from forwarding on all ports.
 ebtables -A FORWARD -p 0x8906 -j DROP
 ebtables -A FORWARD -p 0x8914 -j DROP
+# stop ICMPv6 neighbor solicitation & advertisement packets from forwarding on all ports.
+ebtables -A FORWARD -p ipv6 --ip6-proto ipv6-icmp --ip6-icmp-type neighbour-solicitation -j DROP
+ebtables -A FORWARD -p ipv6 --ip6-proto ipv6-icmp --ip6-icmp-type neighbour-advertisement -j DROP
+
+# Avoiding the VRRP MAC destined packets flooding in bridge level, redirecting the packets
+# to the next layer (routing) for further lookup.
+ebtables -t nat -N VRRP
+ebtables -t nat -A PREROUTING -d 00:00:5e:00:01:00/ff:ff:ff:ff:ff:00 -j VRRP
+ebtables -t nat -A VRRP -p IPv4 -j redirect --redirect-target ACCEPT
+
+ebtables -t nat -A VRRP -p IPv6 --ip6-protocol ipv6-icmp -j RETURN
+ebtables -t nat -A VRRP -p IPv6 -j redirect --redirect-target ACCEPT
+
